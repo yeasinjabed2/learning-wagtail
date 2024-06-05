ffb024eb45e679341d755b578b379d10c0bed9cc
Yeasin Jabed <jabed50000@gmail.com>
2024-06-06 00:08:07 +0600

    initial commit

---
+++ mysite/mysite/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-=cfs8)b1&!b^yibfac)$3^gu^55wyt)$4m&-offlmm-98$$93b"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
