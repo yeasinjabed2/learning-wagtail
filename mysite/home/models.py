f67c36f982e57b92b006d64d8c1990d2d2eefde9
Yeasin Jabed <jabed50000@gmail.com>
2024-06-06 01:34:29 +0600

    update model

--- mysite/home/models.py
+++ mysite/home/models.py
@@ -1,7 +1,15 @@
 from django.db import models
 
 from wagtail.models import Page
+from wagtail.fields import RichTextField
+from wagtail.admin.panels import FieldPanel
 
 
 class HomePage(Page):
-    pass
+    body = RichTextField(blank=True)
+    description = models.CharField(max_length=255, blank=True)
+
+    content_panels = Page.content_panels + [
+        FieldPanel('body', classname="full"),
+        FieldPanel('description'),
+    ]
